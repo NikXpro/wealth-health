bf4dc1da301f78fc221406914aeaf6b191a48f38
NikX <41104633+NikXpro@users.noreply.github.com>
2025-04-01 18:47:38 +0200

    feat(home): add Home component and styling - create Home component for employee creation - add styling for add-employee class to center content

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import "./Home.scss";
+export function Home() {
+  return (
+    <>
+      <div className="add-employee">
+        <h2>Create Employee</h2>
+        <form></form>
+      </div>
+    </>
+  );
+}
