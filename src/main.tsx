7bfb282eea9cde6220cc0012bf0c1a529341fdec
NikX <41104633+NikXpro@users.noreply.github.com>
2025-04-28 19:06:59 +0200

    feat(main): add routing for employee management - implement BrowserRouter for navigation - create routes for EmployeeList and CreateEmployee pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.scss";
-import { Home } from "./pages/Home";
+import { CreateEmployee } from "./pages/CreateEmployee/CreateEmployee";
+import EmployeeList from "./pages/EmployeeList/EmployeeList";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<EmployeeList />} />
+        <Route path="/create-employee" element={<CreateEmployee />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
